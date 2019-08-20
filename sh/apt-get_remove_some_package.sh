6d5529755e27968e0a5a273c7d5897287ee726a9
Overcomer <michael31703@gmail.com>
2019-08-20 21:11:19 +0800

    comment remove python2.7

--- sh/apt-get_remove_some_package.sh
+++ sh/apt-get_remove_some_package.sh
@@ -2,6 +2,6 @@
 # apt-get purge can clearly clean , include setting files.
 sudo apt-get -y purge wolfram-engine
 sudo apt-get -y purge libreoffice*
-sudo apt-get -y purge python2.7*
+#sudo apt-get -y purge python2.7*
 
 echo "apt-get_remove_some_package.sh execute finish !"
