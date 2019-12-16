61eef442be260eeba90eca02909ee43ef8774bda
Michael07220823 <michael31703@gmail.com>
2019-12-16 13:46:20 +0800

    Change python3.5 to python3.7

--- sh/link_opencv4_in_to_your_python3.sh
+++ sh/link_opencv4_in_to_your_python3.sh
@@ -1,5 +1,5 @@
 # Create a cv2.cpython-35m-arm-linux-gnueabihf.so shortcut to /usr/lib/python3/dist-packages/cv2.so.
-sudo ln -s /usr/local/python/cv2/python-3.5/cv2.cpython-35m-arm-linux-gnueabihf.so /usr/lib/python3/dist-packages/cv2.so
+sudo ln -s /usr/local/python/cv2/python-3.7/cv2.cpython-37m-arm-linux-gnueabihf.so /usr/lib/python3/dist-packages/cv2.so
 
 echo "link_opencv4_in_to_your_python3.sh finish!"
 echo "now use 'python3' command to enter python and 'import cv2' then keyin 'cv2.__version__'."
