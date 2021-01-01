4354f6cd29c1069b219457f38fef194df9ad39eb
Michael07220823 <michael31703@gmail.com>
2021-01-01 10:51:16 +0800

    Source .bashrc.

--- sh/make_virtualenv.sh
+++ sh/make_virtualenv.sh
@@ -1,5 +1,5 @@
-# Read ~/.profile and  path variable be updated.
-source $HOME/.profile
+# Read ~/.bashrc and  path variable be updated.
+source $HOME/.bashrc
 # Create python virtual environment and specified interpreter with python3.
 mkvirtualenv py3cv4 -p python3
 # Activate py3cv4 virtual environment.
@@ -10,4 +10,4 @@ pip3 install numpy
 deactivate
 
 echo "make_virtualenv.sh finish!"
-echo "you need to 'source ~/.profile' again, if you want to use py3cv4 virtual environment."
+echo "you need to 'source ~/.bashrc' again, if you want to use py3cv4 virtual environment."
