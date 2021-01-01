671c7f75c4217e3bc32d637c9cfbb27d13366a09
Michael07220823 <michael31703@gmail.com>
2021-01-01 11:48:42 +0800

    Fix python version number error.

--- sh/link_opencv4_in_to_your_python3_virtual_environment.sh
+++ sh/link_opencv4_in_to_your_python3_virtual_environment.sh
@@ -1,5 +1,5 @@
 # Create a cv2.cpython-37m-arm-linux-gnueabihf.so shortcut to ~/.virtualenvs/py3cv4/lib/python3.7/site-packages/cv2.so
-ln -s /usr/local/python/cv2/python-3.5/cv2.cpython-37m-arm-linux-gnueabihf.so ~/.virtualenvs/py3cv4/lib/python3.7/site-packages/cv2.so
+ln -s /usr/local/python/cv2/python-3.7/cv2.cpython-37m-arm-linux-gnueabihf.so ~/.virtualenvs/py3cv4/lib/python3.7/site-packages/cv2.so
 echo "link_opencv_4_in_to_your_python3_virtua_environment.sh finish !"
 echo "in terminal keyin 'workon py3cv4' to activate virtual environment"
 echo "and use 'python3' command to enter python and 'import cv2' then keyin 'cv2.__version__.'"
